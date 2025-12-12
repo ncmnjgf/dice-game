3eba00e06ef97f25e6585575196b93f030a60763
Dadaso <bandgardadaso47@gmail.com>
2025-12-12 21:57:44 +0530

    Approve build scripts for vercel

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import GamePlay from './components/GamePlay.jsx'
-import StartGame from './components/StartGame.jsxtartgame'
+import StartGame from './components/StartGame.jsx'
 import NumberSelector from './components/NumberSelector'
 import './App.css'
 
@@ -8,22 +8,22 @@ function App() {
   // useing use state for hiding and show next screen 
   // it will hide the component of <start/> and replace it to other
 
-  const [isgame,setgame] = useState(false);
+  const [isgame, setgame] = useState(false);
 
-  const togglegame = () =>{
+  const togglegame = () => {
     setgame(prev => !prev);
   }
-  return(
+  return (
     <>
-   {isgame ? <GamePlay/> : <StartGame 
-   toggle ={togglegame}
-   />}
+      {isgame ? <GamePlay /> : <StartGame
+        toggle={togglegame}
+      />}
     </>
 
 
 
   )
-  
+
 }
 
 export default App
